a95751a9a86f3d83af15fbe097f7ca83c57d80d5
Akshat Dubey <akshatdubey0808@gmail.com>
2024-05-24 00:25:18 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,85 @@
+import { Button } from "@radix-ui/themes";
+import { LockIcon, UserIcon, WaypointsIcon } from "lucide-react";
 import Image from "next/image";
+const features = [
+  {
+    id: 1,
+    title: "Free and Open Source",
+    icon: <WaypointsIcon />,
+    description:
+      "Zipit is completely free and open source. You can use it for free and contribute to the project.",
+  },
+  {
+    id: 2,
+    title: "No Account Required",
+    icon: <UserIcon />,
+    description:
+      "Zipit doesn't require any signups. You can create a room and share it with anyone.",
+  },
+  {
+    id: 3,
+    title: "Secure",
+    icon: <LockIcon />,
+    description:
+      "No file is saved beyond the lifetime of a room, so you don't have to worry about your files being spread around the internet.",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="bg-gray-100 w-full h-full min-h-screen gap-8 px-16">
+      <nav className="h-16 border-b fixed w-full left-0 flex items-center justify-center">
+        <Button variant="surface">Create Room</Button>
+      </nav>
+      <div className="w-full grid grid-cols-2 pt-36">
+        <div className="flex flex-col justify-center col-span-1">
+          <div className="pr-4 pl-2 py-1 w-fit hover:bg-gray-300 transition-colors bg-gray-200 text-sm font-medium rounded-full flex gap-3">
+            <span className="bg-primary py-1 h-full px-4 rounded-full text-sm text-white">
+              Guess what?
+            </span>
+            <p className="my-auto">It&apos;s Completely Free!</p>
+          </div>
+          <div className="mt-3">
+            <h1 className="font-extrabold text-blue-950 text-6xl max-w-3xl tracking-tight leading-none">
+              Share files without loosing quality
+            </h1>
+          </div>
+
+          <h5 className="mt-3 text-xl font-normal text-gray-500 max-w-screen-md">
+            Zipit allows you to seamlesslly upload and download files from any
+            device. It&apos;s a super convenient way to transfer files between
+            devices with zero signups
+          </h5>
+          <div className="mt-3">
+            <Button variant="soft" size={"3"}>
+              Create Room
+            </Button>
+          </div>
+        </div>
+        <div className="col-span-1 flex items-center justify-center">
+          <Image
+            src={"/dummy.png"}
+            width={1920}
+            height={1080}
+            alt="Dummy"
+            className="shadow-xl rounded-xl aspect-video"
+          />
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="mt-20 pb-8 grid grid-cols-3 gap-3">
+        {features.map((feature) => (
+          <div key={feature.id} className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <h3 className="text-xl flex items-center gap-2 font-bold text-blue-950">
+                {feature.icon} {feature.title}
+              </h3>
+              <p className="text-md pr-6 font-medium text-gray-500">
+                {feature.description}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section>
   );
 }
