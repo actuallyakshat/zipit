2a4370cb8e51e319b68165fc3ab1f8b2e482620a
Akshat Dubey <akshatdubey0808@gmail.com>
2024-05-30 16:56:06 +0530

    improved ui for smaller screens

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 h-20 w-full bg-zinc-800 text-white">
+    <footer className="fixed bottom-0 left-0 h-20 w-full bg-zinc-800 px-6 text-white">
       <div className="mx-auto flex h-full w-full max-w-screen-xl items-center justify-between">
         <h1 className="text-sm text-zinc-300">
           Created by{" "}
